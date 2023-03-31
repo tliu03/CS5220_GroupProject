0325526c5aef4658bd14dbd92ad9472c737d3c96
luochengzhu <distinctel@gmail.com>
2023-03-30 18:28:04 -0700

    User SignUp and Login screen

--- SnowRide/App.js
+++ SnowRide/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
