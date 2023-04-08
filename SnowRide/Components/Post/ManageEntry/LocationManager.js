9b07452323639863c8618adb5208f60930816188
王欣喏 <wangxinnuo@wangxinnuodeMacBook-Air.local>
2023-04-07 21:53:23 -0700

    added location/camera use files

---
+++ SnowRide/Components/Post/ManageEntry/LocationManager.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+// This function should allow user to either locate their current location
+// or select location off a map when adding a new post // imported under PostForm
+
+export default function LocationManager() {
+  return (
+    <View>
+      <Text>LocationManager</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ SnowRide/Components/User/ImageManager.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+// this allow user to either select pic from camera roll / take picture
+// as their profile image. -- should be saved to firebase storage pls write appropriate
+// functions under Firebase folders
+export default function ImageManager() {
+  return (
+    <View>
+      <Text>ImageManager</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
