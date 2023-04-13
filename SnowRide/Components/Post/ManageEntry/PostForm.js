d89cc73c8b1d9847df1de9693ba9b24b12bf9d96
jingjing515 <majingjing515@outlook.com>
2023-04-12 19:26:53 -0700

    remove modal

--- SnowRide/Components/Post/ManageEntry/PostForm.js
+++ SnowRide/Components/Post/ManageEntry/PostForm.js
@@ -80,110 +80,120 @@ export default function PostForm({ route, navigation }) {
 
   return (
     // <Modal>
-    <View style={styles.Card}>
-      <IconButton
-        name="close-circle-outline"
-        size={18}
-        onPress={returnToPostHandler}
-      />
-      <View style={styles.contentContainer}>
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>
-            {post.category === "driver" ? (
-              <Text>Add Driver Post</Text>
-            ) : (
-              <Text>Add Passenger Post</Text>
-            )}
-          </Text>
-        </View>
-        <View>
-          <Input
-            label="Date"
-            timePicker={true}
-            textInputConfig={{
-              date: postEntry.date,
-              mode: "datetime",
-              format: "YYYY-MM-DD @HH:mm",
-              onDateChange: entryInputHandler.bind(this, "date"),
-              useNativeDriver: true,
-              value: postEntry.date,
-            }}
-          />
-          <Input
-            label="Destination"
-            inputBox={true}
-            textInputConfig={{
-              onChangeText: entryInputHandler.bind(this, "destination"),
-              value: postEntry.destination,
-            }}
-          />
-          <Input
-            label="Pick Up Location"
-            inputBox={true}
-            textInputConfig={{
-              onChangeText: entryInputHandler.bind(this, "pickupLocation"),
-              value: postEntry.pickupLocation,
-            }}
-          />
-          <LocationManager />
-          {post.category === "driver" && (
+    <View style={styles.container}>
+      <View style={styles.Card}>
+        <IconButton
+          name="close-circle-outline"
+          size={18}
+          onPress={returnToPostHandler}
+        />
+        <View style={styles.contentContainer}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>
+              {post.category === "driver" ? (
+                <Text>Add Driver Post</Text>
+              ) : (
+                <Text>Add Passenger Post</Text>
+              )}
+            </Text>
+          </View>
+          <View>
             <Input
-              label="Price per Person"
-              inputBox={true}
+              label="Date"
+              timePicker={true}
               textInputConfig={{
-                keybordType: "decimal-pad",
-                onChangeText: entryInputHandler.bind(this, "price"),
-                value: postEntry.price,
+                date: postEntry.date,
+                mode: "datetime",
+                format: "YYYY-MM-DD @HH:mm",
+                onDateChange: entryInputHandler.bind(this, "date"),
+                useNativeDriver: true,
+                value: postEntry.date,
               }}
             />
-          )}
-          <Input
-            label={
-              post.category === "driver" ? "Spots Available" : "Seats Needed"
-            }
-            inputBox={true}
-            textInputConfig={{
-              keybordType: "numeric",
-              onChangeText: entryInputHandler.bind(this, "availableSpots"),
-              value: postEntry.availableSpots,
-            }}
-          />
-          <Input
-            label={
-              post.category === "driver"
-                ? "Room for Equipment"
-                : "Need Room for Equipment"
-            }
-            optionBox={true}
-            textInputConfig={{
-              onSelect: entryInputHandler.bind(this, "equipmentRoom"),
-              value: postEntry.equipmentRoom,
-            }}
-          />
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button onPress={resetFormHandler} style={styles.buttonStyle}>
-            Reset
-          </Button>
-          {post.id ? (
-            <Button onPress={submitChangeHanlder} style={styles.buttonStyle}>
-              Submit Change
+            <Input
+              label="Destination"
+              inputBox={true}
+              textInputConfig={{
+                onChangeText: entryInputHandler.bind(this, "destination"),
+                value: postEntry.destination,
+              }}
+            />
+            <Input
+              label="Pick Up Location"
+              inputBox={true}
+              textInputConfig={{
+                onChangeText: entryInputHandler.bind(this, "pickupLocation"),
+                value: postEntry.pickupLocation,
+              }}
+            />
+            <LocationManager />
+            {post.category === "driver" && (
+              <Input
+                label="Price per Person"
+                inputBox={true}
+                textInputConfig={{
+                  keybordType: "decimal-pad",
+                  onChangeText: entryInputHandler.bind(this, "price"),
+                  value: postEntry.price,
+                }}
+              />
+            )}
+            <Input
+              label={
+                post.category === "driver" ? "Spots Available" : "Seats Needed"
+              }
+              inputBox={true}
+              textInputConfig={{
+                keybordType: "numeric",
+                onChangeText: entryInputHandler.bind(this, "availableSpots"),
+                value: postEntry.availableSpots,
+              }}
+            />
+            <Input
+              label={
+                post.category === "driver"
+                  ? "Room for Equipment"
+                  : "Need Room for Equipment"
+              }
+              optionBox={true}
+              textInputConfig={{
+                onSelect: entryInputHandler.bind(this, "equipmentRoom"),
+                value: postEntry.equipmentRoom,
+              }}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button onPress={resetFormHandler} style={styles.buttonStyle}>
+              Reset
             </Button>
-          ) : (
-            <Button onPress={submitFormHanlder} style={styles.buttonStyle}>
-              Submit
-            </Button>
-          )}
+            {post.id ? (
+              <Button onPress={submitChangeHanlder} style={styles.buttonStyle}>
+                Submit Change
+              </Button>
+            ) : (
+              <Button onPress={submitFormHanlder} style={styles.buttonStyle}>
+                Submit
+              </Button>
+            )}
+          </View>
         </View>
       </View>
+      {/* // </Modal> */}
     </View>
-    // </Modal>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 10,
+    marginTop: 5,
+  },
   Card: {
-    marginHorizontal: 30,
+    marginHorizontal: 20,
     marginVertical: 100,
     backgroundColor: Colors.primary100,
     borderRadius: 8,
